0ff5d47df853d9417a7de465bc38fe9c5c4881ed
Mfbeeck <mfbeeck@gmail.com>
2015-08-04 01:31:13 -0400

    fixed notes method call

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -65,7 +65,7 @@ class NotesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_note
-      @note = current_user.find(params[:id])
+      @note = current_user.notes.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
